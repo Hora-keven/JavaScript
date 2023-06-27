af1c9e872073a9f21e7105b3005e937dd6dc35e2
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-06-27 14:15:36 -0300

    Add files via upload

---
+++ javascript_study/js.js
@@ -0,0 +1,15 @@
+function mensage(){
+    texto = document.querySelector('#msg')
+    texto.innerHTML = '<p>FUNCIONOU! Testando</p>'
+    texto.style.background = "black"
+    texto.style.color = 'white'
+    texto.style.marginTop = '10px'
+    
+    
+    sect = document.querySelector("section")
+    sect.style.alignItems = 'center'
+
+    area = document.querySelector("#area")
+
+    
+}
