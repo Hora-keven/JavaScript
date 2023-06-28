87dfb3e0cf0097642b29d08ec157234af20cbb52
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-06-27 21:50:03 -0300

    Add files via upload

--- javascript_study/js.js
+++ javascript_study/js.js
@@ -1,15 +1,32 @@
+let valor = 0;
+va = document.querySelector('#valor')
+
 function mensage(){
     texto = document.querySelector('#msg')
     texto.innerHTML = '<p>FUNCIONOU! Testando</p>'
     texto.style.background = "black"
     texto.style.color = 'white'
     texto.style.marginTop = '10px'
-    
-    
-    sect = document.querySelector("section")
-    sect.style.alignItems = 'center'
+    texto.style.width = '50%'
+    texto.style.marginLeft = '25%'
 
-    area = document.querySelector("#area")
 
-    
+    bod = document.querySelector('body')
+    bod.style.background = 'black'
+}
+
+function adiciona(){
+    v = document.querySelector('#mais')
+    valor++;
+    va.innerHTML = `<p>${valor}</p>`;
+    if(valor == 40){
+        va.style.color = 'green'
+    }
+ 
+}
+function diminui(){
+    vn = document.querySelector("#menos")
+    let negativo = valor--;
+    va.innerHTML = `<p>${negativo}</p>`;
+
 }
